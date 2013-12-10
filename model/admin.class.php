6d5b9f7392f02afa7bddc80bb9babe88b65463d4
mayas Haddad <mayas_91@hotmail.com>
2013-12-10 13:54:03 +0100

    added the admin model class

---
+++ model/admin.class.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* This model is responsible of admin data management (using PDO)
+* @author Mayas Haddad
+*/
+class Admin
+{
+	protected $dataBaseConnection;
+
+	function __construct()
+	{
+		$this->dataBaseConnection = DatabaseManager::getDatabaseConnection();
+	}
+
+	function getAdminData($email, $password)
+	{
+		$statement = $this->dataBaseConnection->prepare("SELECT * FROM admin WHERE email = :email AND password = :password");
+		$statement->execute(array(':email' => $email, ':password' => md5($password)));
+		
+		return $statement->fetch();
+	}
+}
+/* This script creates a new admin 
+$dbc = DatabaseManager::getDatabaseConnection();
+$dbc->exec("INSERT INTO admin(email, password) VALUES('admin@example.com', '" . md5('pass') . "')");
+*/
