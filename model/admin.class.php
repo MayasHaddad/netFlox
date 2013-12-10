a11ae42aeddd6a2ede0fa3438a5938fb3a11f2b0
mayas Haddad <mayas_91@hotmail.com>
2013-12-10 16:51:15 +0100

    DEBUG: now checking that there is exactly one admin corresponding to the input

--- model/admin.class.php
+++ model/admin.class.php
@@ -16,8 +16,16 @@ class Admin
 	{
 		$statement = $this->dataBaseConnection->prepare("SELECT * FROM admin WHERE email = :email AND password = :password");
 		$statement->execute(array(':email' => $email, ':password' => md5($password)));
-		
+
 		return $statement->fetch();
+	}
+
+	function getNumberOfAdmins($email, $password)
+	{
+		$statement = $this->dataBaseConnection->prepare("SELECT COUNT(*) FROM admin WHERE email = :email AND password = :password");
+		$statement->execute(array(':email' => $email, ':password' => md5($password)));
+
+		return (int) $statement->fetch()[0];
 	}
 }
 /* This script creates a new admin 
