2bd4c08f8dc9fe3ce84975371966ac88e91a9be1
mayas Haddad <mayas_91@hotmail.com>
2014-01-09 17:51:35 +0100

    updated database generating script

--- data/netflox.sql
+++ data/netflox.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Mar 03 Décembre 2013 à 15:38
+-- Généré le: Jeu 09 Janvier 2014 à 16:49
 -- Version du serveur: 5.6.12-log
 -- Version de PHP: 5.4.12
 
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `admin` (
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id_admin`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=6 ;
 
 -- --------------------------------------------------------
 
@@ -44,15 +44,15 @@ CREATE TABLE IF NOT EXISTS `admin` (
 
 CREATE TABLE IF NOT EXISTS `customer` (
   `id_customer` int(10) NOT NULL AUTO_INCREMENT COMMENT 'primary_key',
-  `login` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `lastname` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `firstname` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `login` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firstname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `credit` bigint(20) NOT NULL,
   PRIMARY KEY (`id_customer`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin7 COLLATE=latin7_bin AUTO_INCREMENT=5 ;
 
 -- --------------------------------------------------------
 
@@ -63,25 +63,35 @@ CREATE TABLE IF NOT EXISTS `customer` (
 CREATE TABLE IF NOT EXISTS `movie_buy_transaction` (
   `id_customer` int(10) NOT NULL,
   `id_movie` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `id_offerer` int(10) DEFAULT NULL,
   `date` date NOT NULL,
-  KEY `id_customer` (`id_customer`),
-  KEY `id_offerer` (`id_offerer`)
+  `price` int(11) NOT NULL,
+  UNIQUE KEY `id_customer_2` (`id_customer`,`id_movie`,`date`,`price`),
+  UNIQUE KEY `id_customer_3` (`id_customer`,`id_movie`,`price`),
+  KEY `id_customer` (`id_customer`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `movie_rent_transaction`
+-- Structure de la table `offered_credit`
 --
 
-CREATE TABLE IF NOT EXISTS `movie_rent_transaction` (
-  `id_customer` int(10) NOT NULL,
-  `id_movie` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remaining_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  UNIQUE KEY `id_customer` (`id_customer`),
-  KEY `id_customer_2` (`id_customer`),
-  KEY `id_customer_3` (`id_customer`)
+CREATE TABLE IF NOT EXISTS `offered_credit` (
+  `id_generous_customer` int(11) NOT NULL,
+  `id_lucky_customer` int(11) NOT NULL,
+  `amount_offered` int(11) NOT NULL,
+  KEY `id_generous_customer` (`id_generous_customer`,`id_lucky_customer`),
+  KEY `id_lucky_customer` (`id_lucky_customer`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `unique_id`
+--
+
+CREATE TABLE IF NOT EXISTS `unique_id` (
+  `unique_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -92,8 +102,14 @@ CREATE TABLE IF NOT EXISTS `movie_rent_transaction` (
 -- Contraintes pour la table `movie_buy_transaction`
 --
 ALTER TABLE `movie_buy_transaction`
-  ADD CONSTRAINT `movie_buy_transaction_ibfk_1` FOREIGN KEY (`id_customer`) REFERENCES `customer` (`id_customer`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `movie_buy_transaction_ibfk_2` FOREIGN KEY (`id_offerer`) REFERENCES `customer` (`id_customer`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `movie_buy_transaction_ibfk_1` FOREIGN KEY (`id_customer`) REFERENCES `customer` (`id_customer`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `offered_credit`
+--
+ALTER TABLE `offered_credit`
+  ADD CONSTRAINT `offered_credit_ibfk_1` FOREIGN KEY (`id_generous_customer`) REFERENCES `customer` (`id_customer`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `offered_credit_ibfk_2` FOREIGN KEY (`id_lucky_customer`) REFERENCES `customer` (`id_customer`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
