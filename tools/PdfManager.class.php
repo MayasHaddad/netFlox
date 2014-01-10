56f61b6bb4739d04a705d4fb599dcc95bf5c1c33
mayas Haddad <mayas_91@hotmail.com>
2014-01-10 11:51:59 +0100

    hotfixes

--- tools/PdfManager.class.php
+++ tools/PdfManager.class.php
@@ -15,8 +15,17 @@ class PdfManager
         $pdf = new FPDF();
         $pdf->AddPage();
         $pdf->SetFont('Arial','B',10);
-        $pdf->BasicTable($header, $moviesData);
-        $pdf->Output();   
+        $moviesString = "";
+        foreach ($moviesData as $key => $movie) 
+        {
+            foreach ($movie as $key1 => $value) 
+            {
+                $pdf->MultiCell(180, 10, $key1 . ' : ' . $value);
+                $pdf->Ln();
+            }  
+            $pdf->AddPage();  
+        }
+        $pdf->Output();
     }
 
     public function getAuditPdf($header, $customerData, $period, $turnover)
